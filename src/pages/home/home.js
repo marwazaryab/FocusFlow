4079840e7cfd49d17fe35d7917f8a2ae0a233e5f
Salma El Gohary <salmaelgohary03@gmail.com>
2025-01-12 17:05:00 -0500

    updated logo

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,13 +5,19 @@ function Home() {
   return (
     <div className="home-page">
       <video 
-        className="intro_screen" 
+        className="intro_screen" //clas name is for css name to style section
         src="/intro_screen.mov" // TODO switch to mp4 file for more accomidation
         autoPlay 
         loop 
         muted 
         type="video/mov"
-        style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover'}}
+        style={{position: 'absolute', top: 70, left: 0, width: '100%', height: '100%', objectFit: 'cover'}}
+      />
+      <img
+        className="logo"
+        src="/logo.png"
+        alt="FocusFlow"
+        style={{position: 'absolute', top: '10px', left: '10px', width: '50px', height: 'auto', objectFit: 'contain'}}
       />
       <div className="content">
         <h1>FocusFlow!</h1>
