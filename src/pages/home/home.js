e7e16ba8fa2692690c4ca7d40d074e6e9978938c
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2025-01-12 17:24:22 -0500

    remove logo from home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -13,12 +13,7 @@ function Home() {
         type="video/mov"
         style={{position: 'absolute', top: 70, left: 0, width: '100%', height: '100%', objectFit: 'cover'}}
       />
-      <img
-        className="logo"
-        src="/logo.png"
-        alt="FocusFlow"
-        style={{position: 'absolute', top: '10px', left: '10px', width: '50px', height: 'auto', objectFit: 'contain'}}
-      />
+
       <div className="content">
         <h1>FocusFlow!</h1>
         <p>Higher Grades, work quality and lower stress.</p>
