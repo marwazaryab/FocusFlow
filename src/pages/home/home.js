27878435c950b768da7cab20eeeb465a0f9f0826
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2025-01-12 19:09:15 -0500

    button links to respective pages

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import './home.css'; 
+import './home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="home-page">
-      <video 
+      <video
         className="intro_screen" //clas name is for css name to style section
         src="/intro_screen.mov" // TODO switch to mp4 file for more accomidation
-        autoPlay 
-        loop 
-        muted 
+        autoPlay
+        loop
+        muted
         type="video/mov"
-        style={{position: 'absolute', top: 70, left: 0, width: '100%', height: '100%', objectFit: 'cover'}}
+        style={{ position: 'absolute', top: 70, left: 0, width: '100%', height: '100%', objectFit: 'cover' }}
       />
 
       <div className="content">
@@ -19,33 +20,32 @@ function Home() {
         <p>Higher Grades, work quality and lower stress.</p>
         <p>Join your AI study buddy and say goodbye to overwhelming days.</p>
 
-        <div className = "button_container">
+        <div className="button_container">
 
-          <button className="study_plan " >
+          <Link to="/study-plan">
+            <button className="study_plan " >
+              <p> Generate your personalized study plan here</p>
+            </button>
+          </Link>
 
-          <p> Generate your personalized study plan here</p>
+          <Link to="/progress_tracker">
+            <button className='progress_tracker'>
+            <p> View your progress tracker here</p>
+            </button>
+          </Link>
 
-          </button>
-
-          <button className='progress_tracker'>
-
-          <p> View your progress tracker here</p>
-
-          </button>
-
-
-          <button className='tracker'>
-
-          <p> View resources on your preferred subject</p>
-          
-          </button>
-
+          <Link to="tracker">
+            <button className='tracker'>
+              <p> View resources on your preferred subject</p>
+            </button>
+          </Link>
 
 
+
+        </div>
       </div>
-      </div>
 
-      
+
 
 
     </div>
