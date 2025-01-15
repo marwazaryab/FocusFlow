4eda5e9c2aa2926b7876734d6944ae89a95b15cc
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2025-01-15 11:38:04 -0500

    creators page and styles

--- src/pages/creators/creators.js
+++ src/pages/creators/creators.js
@@ -1,40 +1,49 @@
 import React from 'react';
 import './creators.css';
 
-function Creators(){
+function Creators() {
     const contributors = [
         {
             id: 1,
             name: "Marwa Zaryab",
-            photo: "/public/marwa.jpg",
+            photo: "marwa.jpg",
         },
         {
             id: 2,
             name: "Salma El Gohary",
-            photo: "/public/salma.jpg",
+            photo: "salma.jpg",
         },
         {
             id: 3,
             name: "Tisiane Wembou",
-            photo: "/public/tisiane.jpg",
+            photo: "tisiane.jpg",
         },
         {
             id: 4,
             name: "Arwa Gawish",
-            photo: "/public/arwa.jpg",
+            photo: "arwa.jpg",
         },
     ];
 
-    return(
-        <div className = "about-us-container">
-            <div className = "content">
+    return (
+        <div className="about-us-container">
+            <div className="content">
                 <h1>About Us</h1>
-                <h2>Innovating Study, Simplifying Success.</h2>
+                <h2>We are 4 Systems Design Engineering students at the University of Waterloo dedicated to transforming the way students approach studying. With a focus on innovation, we strive to make studying more productive and enjoyable for everyone.</h2>
+                
+            <div className="contributors-row">
+                {contributors.map((contributor) => (
+                    <div key={contributor.id} className="contributor-item">
+                        <img
+                            src={contributor.photo}
+                            alt={contributor.name}
+                            className="contributor-photo"
+                        />
+                        <h3>{contributor.name}</h3>
+                    </div>
+                ))}
             </div>
-
-
-        
-
+        </div>
         </div>
     );
 }
